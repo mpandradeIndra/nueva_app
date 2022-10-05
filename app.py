786b6cd674e31da7818800dd1509db5e8d492286
mpandradeIndra <115070146+mpandradeIndra@users.noreply.github.com>
2022-10-05 12:39:50 +0200

    Primera Subida
    Subida de la app y las templates

---
+++ app.py
@@ -0,0 +1,28 @@
+"""
+[Aplicación básica del microframework Flask de Python]
+
+Author: Fortinux
+Date: []
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    
+
+@app.route("/servicios")
+def servicios():
+    return render_template("base.html")
+
+
+@app.route("/contacto")
+def contacto():
+    return render_template("base.html")
+
+
+@app.route("/admin")
+def admin():
+    return render_template("base.html")
